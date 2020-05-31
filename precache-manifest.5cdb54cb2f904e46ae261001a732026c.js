808b607be1ae99e2fca7ebe6efd96d12efc3505d
15077693d <15077693d@gmail.com>
2020-05-31 18:26:40 +0800

    Updates

---
+++ precache-manifest.5cdb54cb2f904e46ae261001a732026c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c7bf4182f3577ad21e03b2e1b5fe2d",
+    "url": "/master-rolodex/index.html"
+  },
+  {
+    "revision": "ea6e2127b7dabe37ca09",
+    "url": "/master-rolodex/static/css/main.660b2945.chunk.css"
+  },
+  {
+    "revision": "ac73105f93c1a7f694cc",
+    "url": "/master-rolodex/static/js/2.0e987918.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/master-rolodex/static/js/2.0e987918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6e2127b7dabe37ca09",
+    "url": "/master-rolodex/static/js/main.6fc528e2.chunk.js"
+  },
+  {
+    "revision": "1224f760e3f2fe50c1f3",
+    "url": "/master-rolodex/static/js/runtime-main.2deb060f.js"
+  }
+]);
